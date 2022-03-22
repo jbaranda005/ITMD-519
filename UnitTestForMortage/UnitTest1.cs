2b342775a65128b1b16a93c3b2fec4ecdfe5d613
jbaranda005 <jbaranda005@ikasle.ehu.eus>
2022-03-22 18:29:20 -0500

    Initial check-in of module Project1

---
+++ UnitTestForMortage/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MortgageHelperLibrary;
+
+namespace UnitTestForMortgage
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMortagage()
+        {
+            var result = MortgageCalcHelper.ComputeMonthlyPayment(1000, 15, 5);
+            Assert.AreEqual(7.91, result);
+
+        }
+            
+    }
+}
